7cfd9bd089b6c820c62507b8c0e77ba6d8972e4d
helianthuslulu <helianthus.lu@gmail.com>
2013-12-03 17:03:31 +0800

    Update and rename mythread.h to producer.h

---
+++ producer.h
@@ -0,0 +1,20 @@
+﻿#ifndef PRODUCER_H
+#define PRODUCER_H
+
+#include <QThread>
+#include <QSemaphore>
+
+const int DataSize = 10000;
+const int BufferSize = 8192;
+int buffer[BufferSize];
+QSemaphore freeBytes(BufferSize);
+QSemaphore usedBytes(0);
+
+class Producer : public QThread
+{
+public:
+    Producer();
+    void run();
+};
+
+#endif // PRODUCER_H
