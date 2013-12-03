d77381940d75d4429fa64c5ed40d61ae723a7964
helianthuslulu <helianthus.lu@gmail.com>
2013-12-03 16:57:14 +0800

    Update and rename dialog.cpp to customer.cpp

---
+++ customer.cpp
@@ -0,0 +1,14 @@
+﻿#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <QThread>
+#include "producer.h"
+
+class Customer : public QThread
+{
+public:
+    Customer();
+    void run();
+};
+
+#endif // CUSTOMER_H
