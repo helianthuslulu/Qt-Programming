06e36759a1c21ee3886a1589d2e0ee678126c561
helianthuslulu <helianthus.lu@gmail.com>
2013-12-03 17:13:25 +0800

    Update and rename dialog.cpp to timeclient.h

---
+++ timeclient.h
@@ -0,0 +1,46 @@
+﻿#ifndef TIMECLIENT_H
+#define TIMECLIENT_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QDateTimeEdit>
+#include <QtNetwork/QTcpSocket>
+#include <QtNetwork/QAbstractSocket>
+
+namespace Ui {
+class TimeClient;
+}
+
+class TimeClient : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit TimeClient(QWidget *parent = 0);
+    ~TimeClient();
+
+public slots:
+    void enableGetBtn();
+    void getTime();
+    void readTime();
+    void showError(QAbstractSocket::SocketError socketError);
+    
+private:
+    Ui::TimeClient *ui;
+    QLabel * serverNameLabel;
+    QLineEdit * serverNameLineEdit;
+    QLabel * portLabel;
+    QLineEdit * portLineEdit;
+    QDateTimeEdit * dateTimeEdit;
+    QLabel * stateLabel;
+
+    QPushButton * getbtn;
+    QPushButton * quitbtn;
+
+    uint time2u;
+    QTcpSocket * tcpSocket;
+};
+
+#endif // TIMECLIENT_H
